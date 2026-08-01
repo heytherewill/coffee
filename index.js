0396d9b1ac46f787552337e4198e60d4271af32f
William Barbosa <heytherewill@gmail.com>
2026-08-01 21:24:00 +0200

    🔲 List scaffold

--- index.js
+++ index.js
@@ -12,7 +12,20 @@
         for(const recipe of recipesInDisplay) {
             // TODO: Create actual render of each recipe.
             const recipeElement = document.createElement('li');
-            recipeElement.innerHTML = `<h1>${recipe.name}</h1> <p>${recipe.instructions}</p>`;
+            recipeElement.innerHTML =
+`
+                <div class="recipeHeader">
+                    <img src="https://placehold.co/60x60"/>
+                    <div>
+                    <h3>${recipe.name}</h3>
+                        <h5>${recipe.grounds}gr . ${recipe.water}ml . ${recipe.timeInSeconds} seconds</h5>
+                        <h4>Show instructions</h4>
+                    </div>
+                </div>
+                <div class="instructions">
+                    ${recipe.instructions}
+                </div>`
+
             recipeList.appendChild(recipeElement);
         }
     }
