82c525a169b16d72a5a050f4a5304ffa3ca22211
William Barbosa <heytherewill@gmail.com>
2026-08-01 19:53:48 +0200

    💅🏽 Render recipes

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-(function() {
+(async function main() {
+    const response = await fetch("/recipes.json");
+    const allRecipes = await response.json();
+    var recipesInDisplay = allRecipes;
+    renderRecipes();
+
+    function renderRecipes() {
+        const recipesContainer = document.querySelector("#recipes");
+        // Clean everything inside the container.
+        recipesContainer.innerHTML = '<ul id="recipeList"></ul>';
+        const recipeList = recipesContainer.querySelector("#recipeList");
+        for(const recipe of recipesInDisplay) {
+            // TODO: Create actual render of each recipe.
+            const recipeElement = document.createElement('li');
+            recipeElement.innerHTML = `<h1>${recipe.name}</h1> <p>${recipe.instructions}</p>`;
+            recipeList.appendChild(recipeElement);
+        }
+    }
 })();
