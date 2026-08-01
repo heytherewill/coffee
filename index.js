bd1a691742b602d6cfa4ec6220205d01c12f0b3b
William Barbosa <heytherewill@gmail.com>
2026-08-01 23:36:33 +0200

    🕐 Format prep time

--- index.js
+++ index.js
@@ -10,7 +10,9 @@
         recipesContainer.innerHTML = '<ul id="recipeList"></ul>';
         const recipeList = recipesContainer.querySelector("#recipeList");
         for (const recipe of recipesInDisplay) {
-            // TODO: Create actual render of each recipe.
+
+            const formattedTime = formatTimeFromSeconds(recipe.timeInSeconds);
+
             const recipeElement = document.createElement('li');
             recipeElement.innerHTML = `
                 <div class="recipeHeader">
@@ -20,7 +22,7 @@
                         <h5>
                             <i class="fa-solid fa-weight-scale"></i>${recipe.grounds}gr
                             <i class="fa-solid fa-droplet"></i>${recipe.water}ml
-                            <i class="fa-solid fa-clock"></i>${recipe.timeInSeconds} seconds</h5>
+                            <i class="fa-solid fa-clock"></i>${formattedTime}</h5>
                         <h4></h4>
                     </div>
                 </div>
@@ -40,6 +42,29 @@
             instructionsContainer.appendChild(showInstructionsLink);
 
             recipeList.appendChild(recipeElement);
+        }
+
+        function formatTimeFromSeconds(seconds) {
+            const secondsInOneHour = 3600;
+            const secondsInOneMinute = 60;
+
+            const hours = Math.floor(seconds / secondsInOneHour);
+            console.log(hours);
+            const remainingSecondsAfterHours = seconds - (hours * secondsInOneHour);
+            console.log(remainingSecondsAfterHours);
+            const minutes = Math.floor(remainingSecondsAfterHours / secondsInOneMinute);
+            console.log(minutes);
+            const remainingSeconds = remainingSecondsAfterHours - (minutes * secondsInOneMinute);
+            console.log(remainingSeconds);
+            
+            const duration = {
+                hours: hours,
+                minutes: minutes,
+                seconds: remainingSeconds
+            };
+
+            const durationFormatter = new Intl.DurationFormat("en", { style: "short" });
+            return durationFormatter.format(duration);
         }
     }
 })();
